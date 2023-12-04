3dc87b244cf1555181c24fa19885a0ec2355743d
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-12-04 10:34:04 +0100

    add rust solution for day 1

---
+++ 2023/rust/dayone/src/part1.rs
@@ -0,0 +1,47 @@
+use std::io::{self, BufRead};
+
+fn to_digit(iter: &mut dyn Iterator<Item = char>) -> Option<u32> {
+    while let Some(v) = iter.next().map(|c| c.to_digit(10)) {
+        if let Some(d) = v {
+            return Some(d);
+        }
+    }
+    None
+}
+
+fn number(s: &str) -> u32 {
+    let first = to_digit(&mut s.chars()).expect("must have first digit");
+    let last = to_digit(&mut s.chars().rev()).expect("must have last digit");
+    first * 10 + last
+}
+
+fn part1<T: Iterator<Item = String>>(it: T) -> u32 {
+    it.map(|line| number(&line)).sum::<u32>()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let iter = stdin.lock().lines().map(|line| line.unwrap());
+    part1(iter);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+    use std::io::{self, BufRead};
+
+    #[test]
+    fn short() {
+        let cursor = io::Cursor::new(
+            vec![
+                "1abc2".to_string(),
+                "pqr3stu8vwx".to_string(),
+                "a1b2c3d4e5f".to_string(),
+                "treb7uchet".to_string(),
+            ]
+            .join("\n"),
+        );
+        let sum = part1(cursor.lines().map(|line| line.unwrap()));
+        assert_eq!(sum, 142);
+    }
+}
---
+++ 2023/rust/dayone/src/part2.rs
@@ -0,0 +1,66 @@
+use std::io::{self, BufRead};
+use std::ops::ControlFlow;
+
+fn first_digit<'a, T>(mut iter: T) -> Option<u32>
+where
+    T: Iterator<Item = (&'a str, u8)>,
+{
+    const DIGITS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let result = iter.try_fold((), |_, (s, b)| {
+        if let Some(digit) = (b as char).to_digit(10) {
+            return ControlFlow::Break(digit);
+        }
+        if let Some(index) = DIGITS.iter().position(|&w| s.starts_with(w)) {
+            return ControlFlow::Break((index as u32) + 1);
+        }
+        ControlFlow::Continue(())
+    });
+    match result {
+        ControlFlow::Break(digit) => Some(digit),
+        ControlFlow::Continue(_) => None,
+    }
+}
+
+fn number(s: &str) -> u32 {
+    let first: u32 = first_digit(s.bytes().enumerate().map(|(i, b)| (&s[i..], b)))
+        .expect("must have first digit");
+    let last: u32 = first_digit(s.bytes().enumerate().rev().map(|(i, b)| (&s[i..], b)))
+        .expect("must have last digit");
+    (first * 10) + last
+}
+
+fn part2<T: Iterator<Item = String>>(it: T) -> u32 {
+    it.map(|line| number(&line)).sum()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let iter = stdin.lock().lines().map(|line| line.unwrap());
+    println!("sum={}", part2(iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part2;
+    use std::io::{self, BufRead};
+
+    #[test]
+    fn short() {
+        let cursor = io::Cursor::new(
+            vec![
+                "two1nine".to_string(),
+                "eightwothree".to_string(),
+                "abcone2threexyz".to_string(),
+                "xtwone3four".to_string(),
+                "4nineeightseven2".to_string(),
+                "zoneight234".to_string(),
+                "7pqrstsixteen".to_string(),
+            ]
+            .join("\n"),
+        );
+        let sum = part2(cursor.lines().map(|line| line.unwrap()));
+        assert_eq!(sum, 281);
+    }
+}
